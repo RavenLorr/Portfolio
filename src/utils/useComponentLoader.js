32c5862ef9675fa601aeffff83a42237c826ce40
Étienne (Ethan) <lolokiepro@gmail.com>
2024-10-08 22:12:50 -0400

    Update dependency

--- src/utils/useComponentLoader.js
+++ src/utils/useComponentLoader.js
@@ -20,7 +20,7 @@ const useComponentLoader = () => {
             }
 
             // Add a small delay to ensure everything is ready
-            await new Promise(resolve => setTimeout(resolve, 500));
+            await new Promise(resolve => setTimeout(resolve, 100));
 
             setIsLoading(false);
         };
