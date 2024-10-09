0e9d18ecfcba44002b6689359848fd59fbafccab
Étienne (Ethan) <lolokiepro@gmail.com>
2024-10-09 19:22:11 -0400

    Projects over Background

--- src/components/subPages/Projects.jsx
+++ src/components/subPages/Projects.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Projects() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold">My Projects</h1>
-            <p className="text-lg text-gray-700 mt-4">This is the projects page.</p>
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+            <div className="block">
+                <h1 className="text-4xl font-bold">My Projects</h1>
+                <p className="text-lg mt-4">This is the projects page.</p>
+            </div>
         </div>
+
     );
 }
 
