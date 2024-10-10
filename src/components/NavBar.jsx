783d729c1fe9d70fa8aed510851d1356f1c11f18
Étienne (Ethan) <lolokiepro@gmail.com>
2024-10-09 20:08:46 -0400

    Addaptative NavBar
    - Import Scaling Factor to NavBar for font-size, height, marging
    - Made sure it's updating in real time with useEffect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CanvasUtils } from '../utils/canvasUtils';
 
 const navItems = [
     { text: 'Home', path: '/' },
@@ -9,10 +10,10 @@ const navItems = [
     { text: 'Experience', path: '/experience' }
 ];
 
-function NavItem({ text, path }) {
+function NavItem({ text, path, scalingFactor }) {
     return (
-        <li>
-            <Link to={path} className="font-space-game text-white hover:underline transition duration-300">
+        <li style={{ margin: `${10 * scalingFactor}px` }}>
+            <Link to={path} className="font-space-game text-white hover:underline transition duration-300" style={{ fontSize: `${22 * scalingFactor}px` }}>
                 {text}
             </Link>
         </li>
@@ -20,11 +21,30 @@ function NavItem({ text, path }) {
 }
 
 function NavBar() {
+    const [scalingFactor, setScalingFactor] = useState(1);
+
+    const updateScalingFactor = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        const { scalingFactor } = CanvasUtils.calculateBaseRadiusAndCenter(canvas);
+        setScalingFactor(scalingFactor);
+    };
+
+    useEffect(() => {
+        updateScalingFactor();
+        window.addEventListener('resize', updateScalingFactor);
+
+        return () => {
+            window.removeEventListener('resize', updateScalingFactor);
+        };
+    }, []);
+
     return (
-        <nav className="fixed top-0 right-0 w-full p-4 flex items-center bg-custom-radial bg-custom-radial-opacity z-50">
+        <nav className="fixed top-0 right-0 w-full p-4 flex items-center bg-custom-radial bg-custom-radial-opacity z-50" style={{ height: `${50 * scalingFactor}px` }}>
             <ul className="flex ml-auto space-x-4">
                 {navItems.map((item) => (
-                    <NavItem key={item.text} {...item} />
+                    <NavItem key={item.text} {...item} scalingFactor={scalingFactor} />
                 ))}
             </ul>
         </nav>
