9df1e97221783b88462698e15239806d9c36f225
Étienne (Ethan) <lolokiepro@gmail.com>
2024-10-29 22:45:56 -0400

    Data Updated (not tested)

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -9,27 +9,32 @@ export const aboutData = {
             basics: {
                 icon: FaUser,
                 title: "The Basics",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum."
+                content: "My name is Étienne, even thought I am commonly reffered to by my English name Ethan. I am a 21-year-old developer born in Québec, Canada. Currently, " +
+                  "I am completing my studies in Application Development while also engaging in various contract and personal projects."
             },
             sport: {
                 icon: FaRunning,
                 title: "Sport",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris."
+                content: "I have a passion for sports, much like many others. During my childhood, I played soccer, and over the years, " +
+                  "I transitioned to basketball, volleyball, climbing, and many others. My most recent and favorite sports discovery remains calisthenics."
             },
             photography: {
                 icon: FaCamera,
                 title: "Photography",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem diam."
+                content: "One of my hobbies is photography. I take great pleasure in capturing moments, landscapes, and the beauty of people. " +
+                  "I am continually learning and refining my skills, much like in my development work, and I take pride in my progress and passion for this art form."
             },
             drawing: {
                 icon: FaPencilAlt,
                 title: "Drawing",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem."
+                content: "I have always had a passion for drawing, a passion I kept since childhood. Although I wasn't particularly skilled at the time," +
+                  " I kept practicing and have seen significant improvement over the years. While drawing remains a hobby, I take great joy in it and am proud of the progress I have made."
             },
             goals: {
                 icon: FaFlag,
                 title: "Goals",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis."
+                content: "I would love expand my knowledge in AI and machine learning, as well as improving my skills in web development. I am also interested in " +
+                  "learning more about game development and design. I consistently seek new challenges and opportunities for learning and growth"
             }
         }
     },
@@ -41,27 +46,32 @@ export const aboutData = {
             basics: {
                 icon: FaUser,
                 title: "Les Bases",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum."
+                content: "Je m'appelle Étienne, bien que je sois normalement appelé par mon nom anglais Ethan. Je suis un développeur de 21 ans, né au Québec, Canada." +
+                  "je complète actuellement mes études en développement d'applications tout en travaillant sur divers projets personnels et contractuels."
             },
             sport: {
                 icon: FaRunning,
                 title: "Sport",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris."
+                content: "J'ai une passion pour le sport, tout comme beaucoup d'autres. Durant mon enfance, j'ai joué au soccer, et au fil des ans, " +
+                  "je suis passé au basketball, au volleyball, à l'escalade, et à bien d'autres. Ma découverte sportive la plus récente et préférée reste encore à ce jour la callisthénie."
             },
             photography: {
                 icon: FaCamera,
                 title: "Photographie",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non lorem diam."
+                content: "Un de mes passe-temps est la photographie. Je prends un grand plaisir à capturer des moments, des paysages, et la beauté des gens. " +
+                  "Je continue d'apprendre et de perfectionner mes compétences, tout comme dans mon travail de développement, et je suis fier de mes progrès et de ma passion pour cet art."
             },
             drawing: {
                 icon: FaPencilAlt,
                 title: "Dessin",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem."
+                content: "J'ai toujours eu une passion pour le dessin, une passion que j'ai gardée depuis l'enfance. Bien que je n'étais pas particulièrement doué à l'époque," +
+                  " j'ai continué à me pratiquer et j'ai vu une amélioration significative au fil des ans. Bien que le dessin reste un passe-temps, je prends un grand plaisir et suis fier des progrès que j'ai réalisés."
             },
             goals: {
                 icon: FaFlag,
                 title: "Objectifs",
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis."
+                content: "J'aimerais approfondir mes connaissances en IA et en apprentissage machine, ainsi qu'améliorer mes compétences en développement web. Je suis également intéressé à " +
+                  "en apprendre plus sur le développement et la conception de jeux. Je cherche constamment de nouveaux défis et des opportunités d'apprentissage et de croissance."
             }
         }
     },
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,33 +1,49 @@
 export const experienceData = {
-    years: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
+    years: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026],
     en: {
         experienceTitle: "Professional Experience",
         educationTitle: "Education",
         experience: [
-            { startYear: 2015, endYear: 2018, title: "Lorem ipsum dolor", subtitle: "Sit amet consectetur", color: "bg-blue-500" },
-            { startYear: 2018, endYear: 2021, title: "Ipsum dolor sit", subtitle: "Amet elit sed", color: "bg-green-500" },
-            { startYear: 2021, endYear: 2025, title: "Dolor sit amet", subtitle: "Consectetur adipiscing elit", color: "bg-purple-500" },
+            { startYear: 2019, endYear: 2019, title: "First Project", subtitle: "Hardcoded interactive adventure game in Batch", color: "bg-teal-500" },
+            { startYear: 2021, endYear: 2021, title: "Minecraft Server", subtitle: "A project taken with a friend where we develop a complete server over a period of a few months", color: "bg-blue-500" },
+            { startYear: 2021, endYear: 2022, title: "Red Dead Redemption 2", subtitle: "Lead Dev on a private RDR2 server developed from scratch (RedM)", color: "bg-green-500" },
+            { startYear: 2022, endYear: 2022, title: "Grand Theft Auto V", subtitle: "Developer and Safety ref on a GTA V private server (FiveM)", color: "bg-yellow-500" },
+            { startYear: 2023, endYear: 2023, title: "Last Defender", subtitle: "Tower Deffence game (Unity) made in 1 month with a college", color: "bg-red-500" },
+            { startYear: 2024, endYear: 2024, title: "Cars Runner", subtitle: "A Top runner mobile game (Unity), created on contract", color: "bg-purple-500" },
         ],
         education: [
-            { startYear: 2015, endYear: 2019, title: "Lorem ipsum dolor", subtitle: "Sit amet consectetur adipiscing", color: "bg-orange-500" },
-            { startYear: 2016, endYear: 2020, title: "Ipsum dolor sit", subtitle: "Amet elit sed do eiusmod", color: "bg-blue-500" },
-            { startYear: 2020, endYear: 2022, title: "Dolor sit amet", subtitle: "Consectetur adipiscing elit sed", color: "bg-red-500" },
-            { startYear: 2023, endYear: 2025, title: "Sit amet consectetur", subtitle: "Lorem ipsum dolor adipiscing", color: "bg-teal-500" },
-        ],
+            { startYear: 2015, endYear: 2020, title: "Secondary School", subtitle: "General Program + Sport (Basketball)", color: "bg-orange-500" },
+            { startYear: 2020, endYear: 2025, title: "College", subtitle: "Computer science technology (Application Development)", color: "bg-pink-500" },
+
+            { startYear: 2020, endYear: 2021, title: "First Year", subtitle: "Learning about OOP (Java), Web Interface and Microsoft tools", color: "bg-indigo-500" },
+            { startYear: 2021, endYear: 2022, title: "Second Year", subtitle: "Learning about OS, network architecture, OOP-2 (Java), Java detailed", color: "bg-lime-500" },
+            { startYear: 2022, endYear: 2023, title: "Third Year", subtitle: "Learning about DB (SQL), server, Web developement (JS/PHP)", color: "bg-cyan-500" },
+            { startYear: 2023, endYear: 2024, title: "Fouth Year", subtitle: "Learning about mathematics of computer, DB-2 (SQL/Maria), Validation and Maintenance, Game Development (Unity)", color: "bg-amber-500" },
+            { startYear: 2024, endYear: 2025, title: "Fifth Year (Current)", subtitle: "Learning about single-user application (C++, java), Web App (js, React), DB-3 (Oracle), Mobile App, Technology Watch", color: "bg-rose-500" },
+            { startYear: 2025, endYear: 2026, title: "Sixth Year", subtitle: "Learning about App security, Game Development-2, Connected object, Application development", color: "bg-fuchsia-500" },
+        ],//bg-emerald-500
     },
     fr: {
         experienceTitle: "Expérience Professionnelle",
         educationTitle: "Éducation",
         experience: [
-            { startYear: 2015, endYear: 2018, title: "Lorem ipsum dolor", subtitle: "Sit amet consectetur", color: "bg-blue-500" },
-            { startYear: 2018, endYear: 2021, title: "Ipsum dolor sit", subtitle: "Amet elit sed", color: "bg-green-500" },
-            { startYear: 2021, endYear: 2025, title: "Dolor sit amet", subtitle: "Consectetur adipiscing elit", color: "bg-purple-500" },
+            { startYear: 2019, endYear: 2019, title: "Premier projet", subtitle: "Jeu d'aventure interactif codé en dur en Batch", color: "bg-teal-500" },
+            { startYear: 2021, endYear: 2021, title: "Serveur Minecraft", subtitle: "Un projet fait avec un ami où nous développions un serveur complet sur une période de quelques mois", color: "bg-blue-500" },
+            { startYear: 2021, endYear: 2022, title: "Red Dead Redemption 2", subtitle: "Dev principal sur un serveur privé RDR2 développé à partir de zéro (RedM)", color: "bg-green-500" },
+            { startYear: 2022, endYear: 2022, title: "Grand Theft Auto V", subtitle: "Développeur et référent sécurité sur un serveur privé GTA V (FiveM)", color: "bg-yellow-500" },
+            { startYear: 2023, endYear: 2023, title: "Last Defender", subtitle: "Jeu de défense de tour (Unity) réalisé en 1 mois avec un collège", color: "bg-red-500" },
+            { startYear: 2024, endYear: 2024, title: "Cars Runner", subtitle: "Un jeu mobile Top runner (Unity), créé sur contrat", color: "bg-purple-500" },
         ],
         education: [
-            { startYear: 2015, endYear: 2019, title: "Lorem ipsum dolor", subtitle: "Sit amet consectetur adipiscing", color: "bg-orange-500" },
-            { startYear: 2016, endYear: 2020, title: "Ipsum dolor sit", subtitle: "Amet elit sed do eiusmod", color: "bg-blue-500" },
-            { startYear: 2020, endYear: 2022, title: "Dolor sit amet", subtitle: "Consectetur adipiscing elit sed", color: "bg-red-500" },
-            { startYear: 2023, endYear: 2025, title: "Sit amet consectetur", subtitle: "Lorem ipsum dolor adipiscing", color: "bg-teal-500" },
+            { startYear: 2015, endYear: 2020, title: "École Secondaire", subtitle: "Programme Général + Sport (BasketBall)", color: "bg-orange-500" },
+            { startYear: 2020, endYear: 2025, title: "Cégep", subtitle: "Technique Informatique (Développement d'Application)", color: "bg-pink-500" },
+
+            { startYear: 2020, endYear: 2021, title: "Première Année", subtitle: "Apprentissage de la POO (Java), Interface Web et outils Microsoft", color: "bg-indigo-500" },
+            { startYear: 2021, endYear: 2022, title: "Deuxième Année", subtitle: "Apprentissage des OS, architecture réseau, POO-2 (Java), Java détaillé", color: "bg-lime-500" },
+            { startYear: 2022, endYear: 2023, title: "Troisième Année", subtitle: "Apprentissage des BD (SQL), serveur, Développement Web (JS/PHP)", color: "bg-cyan-500" },
+            { startYear: 2023, endYear: 2024, title: "Quatrième Année", subtitle: "Apprentissage des mathématiques de l'informatique, BD-2 (SQL/Maria), Validation et Maintenance, Développement de jeux (Unity)", color: "bg-amber-500" },
+            { startYear: 2024, endYear: 2025, title: "Cinquième Année (Actuelle)", subtitle: "Apprentissage des applications monopostes (C++, java), Application Web (js, React), BD-3 (Oracle), Application Mobile, Veille Technologique", color: "bg-rose-500" },
+            { startYear: 2025, endYear: 2026, title: "Sixième Année", subtitle: "Apprentissage de la sécurité des applications, Développement de jeux-2, Objet connecté, Développement d'application", color: "bg-fuchsia-500" },
         ],
     }
 };
